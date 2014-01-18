f84303c038691aafc14c34b29022b27f53472486
mezoni <andrew.mezoni@gmail.com>
2014-01-18 12:59:37 +0600

    0.0.1

--- bin/build_cpp_extension.dart
+++ bin/build_cpp_extension.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
-import 'package:dart_and_cpp_classes/src/project.dart';
+import 'package:dart_and_cpp_classes/src/cpp_extension_project.dart';
 import 'package:path/path.dart' as pathos;
 
 void main() {
   var path = pathos.dirname(Platform.script.path);
   path = pathos.dirname(path);
   path = pathos.join(path, "lib/src");
-  var project = new Project();
-  project.PATH_TO_PROJECT = path;
-  project.run();
+  var project = new CppExtensionProject(path);
+  project.build();
 }
